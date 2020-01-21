bdacb088013fa51dd1ee37e4edfe4b2e2130ce54
Tim Cheng <tcheng13@berkeley.edu>
2020-01-21 17:32:17 -0500

    dynamic page generation

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,42 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { Link, graphql, useStaticQuery} from 'gatsby'
+
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+    
     return (
             <Layout>
                 <h1>Blog</h1>
-                <p>posts will go here</p>
+                <ol>
+                    {data.allMarkdownRemark.edges.map((edge) => {
+                            return (
+                                <Link to={`/blog/${edge.node.fields.slug}`}>
+                                    <li>
+                                        <h2>{edge.node.frontmatter.title}</h2>
+                                        <p>{edge.node.frontmatter.date}</p>
+                                    </li>
+                                </Link>
+                            )
+                    })}
+                </ol>
             </Layout>
     )
 }
